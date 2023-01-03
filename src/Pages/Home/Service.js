342fa105960d997cdf11a06da306598cd4817d90
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-03 16:05:32 +0900

    services section is been updated

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Service = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Service = ({service}) => {
+    const {img, _id, name, discription} = service;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl text-black">
+      <figure className="px-10 pt-10">
+        <img
+          src={img}
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{discription}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,11 +1,45 @@
 import React from "react";
+import Service from "./Service";
+import fluoride from "../../assets/images/fluoride.png"
+import cavity from "../../assets/images/cavity.png"
+import whitening from "../../assets/images/whitening.png"
 
 const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Tretment",
+      discription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem est in, harum distinctio assumenda culpa.",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Filling",
+      discription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem est in, harum distinctio assumenda culpa.",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: "Teeth Whitening",
+      discription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem est in, harum distinctio assumenda culpa.",
+      img: whitening,
+    },
+  ];
   return (
     <div className="my-28">
       <div className="text-center">
-        <h3 className="text-primary text-xl font-bold uppercase">Our services</h3>
+        <h3 className="text-primary text-xl font-bold uppercase">
+          Our services
+        </h3>
         <h2 className="text-4xl">Services We Provide</h2>
+      </div>
+      <div className="grid gird-cols-1 lg:grid-cols-3 pt-12 px-28">
+        {
+            services.map(service => <Service
+            key={service._id}
+            service={service}
+            ></Service>)
+        }
       </div>
     </div>
   );
