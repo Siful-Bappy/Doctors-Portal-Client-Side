9790c4ffaf8998a274b1b81c7269b0a3f200bdaf
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-03 13:16:53 +0900

    router is complete

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <RouterProvider router={router}>
+    </RouterProvider> */}
+    <BrowserRouter>
+    <App></App>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
