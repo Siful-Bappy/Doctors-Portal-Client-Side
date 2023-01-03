78a3fb4c72f0286f92cefe10f77dd70fbae0e6e8
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-03 15:02:35 +0900

    update info card

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import InfoCard from './InfoCard';
-import clock from "../../assets/icons/clock.svg"
-import marker from "../../assets/icons/marker.svg"
-import phone from "../../assets/icons/phone.svg"
+import React from "react";
+import InfoCard from "./InfoCard";
+import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
-    return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={marker}></InfoCard>
-            <InfoCard img={phone}></InfoCard>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      <InfoCard
+        img={clock}
+        text={"Openings Hours"}
+        textDiscription={" form 9:00PM to 6:00PM"}
+        color={"bg-gradient-to-r from-primary to-secondary"}
+      ></InfoCard>
+      <InfoCard
+        img={marker}
+        text={"Visit our location"}
+        textDiscription={
+          "66, Puraton Kalabagan Road, Jhalakati Sadar, Jhalakati"
+        }
+        color={"bg-accent"}
+      ></InfoCard>
+      <InfoCard
+        img={phone}
+        text={"Contact us now"}
+        textDiscription={"+880 1736648283"}
+        color={"bg-gradient-to-r from-primary to-secondary"}
+      ></InfoCard>
+    </div>
+  );
 };
 
 export default Info;
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const InfoCard = ({img}) => {
+const InfoCard = ({img, text, textDiscription, color}) => {
   return (
     <div>
-      <div className="card lg:card-side bg-base-100 shadow-xl bg-accent text-white">
+      <div className={`card lg:card-side bg-base-100 shadow-xl {color} text-white pl-5 ${color}`}>
         <figure>
           <img src={img} alt="Album" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title ">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+          <h2 className="card-title ">{text}</h2>
+          <p>{textDiscription}</p>
         </div>
       </div>
     </div>
