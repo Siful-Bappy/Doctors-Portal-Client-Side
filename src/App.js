c8dd153cec21c778a809245b258fd86f6a25a148
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-03 12:45:44 +0900

    upgrade coustom color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
 function App() {
+  const name = 10;
+  console.log(name)
   return (
     <div>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-third">Button</button>
+      <button class="btn w-64 rounded-full">Button</button>
     </div>
   );
 }
