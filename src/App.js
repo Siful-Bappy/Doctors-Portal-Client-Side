379c4187e10c2ce230339ea549a24b3cc4db22b0
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-03 13:40:38 +0900

    router is complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/Home/About";
 import Home from "./Pages/Home/Home";
-import NavBarLink from "./Pages/Home/NavBarLink";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <NavBarLink></NavBarLink>
       <Routes>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
