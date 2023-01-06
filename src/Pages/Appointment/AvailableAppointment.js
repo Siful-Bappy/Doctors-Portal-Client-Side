4f83f32af2d376a5125d6941ea053654cc5263e0
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-06 17:39:36 +0900

    services.json been added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,12 +1,24 @@
-import { format } from 'date-fns/esm';
-import React from 'react';
+import { format } from "date-fns/esm";
+import React, { useEffect, useState } from "react";
 
-const AvailableAppointment = ({date}) => {
-    return (
-        <div>
-            <h3 className='text-xl text-center text-secondary'>Available Appointments on {format(date, "PP")}</h3>
-        </div>
-    );
+const AvailableAppointment = ({ date }) => {
+  const [services, setServices] = useState([]);
+//   console.log(services);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <h3 className="text-xl text-center text-secondary">
+        Available Appointments on {format(date, "PP")}
+      </h3>
+      <div>
+
+      </div>
+    </div>
+  );
 };
 
 export default AvailableAppointment;
