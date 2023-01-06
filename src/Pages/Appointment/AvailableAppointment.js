7bc856f6cadaaedc070b3591f2e1782f0fcd25f6
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-06 18:08:11 +0900

    appointment available is now showing

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,5 +1,6 @@
 import { format } from "date-fns/esm";
 import React, { useEffect, useState } from "react";
+import Service from "./Service";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
@@ -11,11 +12,16 @@ const AvailableAppointment = ({ date }) => {
   }, []);
   return (
     <div>
-      <h3 className="text-xl text-center text-secondary">
+      <h3 className="text-xl text-center text-secondary mb-20">
         Available Appointments on {format(date, "PP")}
       </h3>
-      <div>
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {
+            services.map(service => <Service
+            key={service._id}
+            service={service}
+            ></Service>)
+        }
       </div>
     </div>
   );
