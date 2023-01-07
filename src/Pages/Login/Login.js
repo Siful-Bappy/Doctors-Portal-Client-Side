8849e86b84d0526732e5f44c9823cd6f3ad37bcb
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-07 17:28:38 +0900

    start the signin things now

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import auth from "../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
