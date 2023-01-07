a239814d201085fc8ae0cabf4695084c69ef590e
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-07 14:50:28 +0900

    login page task is going on

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h3>Login</h3>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+          <div className="divider">OR</div>
+          <button className="btn btn-outline">Continue with Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
