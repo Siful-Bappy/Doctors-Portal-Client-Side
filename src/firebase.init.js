68e44545d604c583c30c3122216d1e255dceda17
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-07 15:51:07 +0900

    google signin is complete

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,22 +5,22 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAkOA0KetEj22ncWP84pjqZoBWOzNjJJXI",
-//   authDomain: "doctors-portal-1cdf0.firebaseapp.com",
-//   projectId: "doctors-portal-1cdf0",
-//   storageBucket: "doctors-portal-1cdf0.appspot.com",
-//   messagingSenderId: "960029723357",
-//   appId: "1:960029723357:web:99bbc469ffea09115fa934",
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyAkOA0KetEj22ncWP84pjqZoBWOzNjJJXI",
+  authDomain: "doctors-portal-1cdf0.firebaseapp.com",
+  projectId: "doctors-portal-1cdf0",
+  storageBucket: "doctors-portal-1cdf0.appspot.com",
+  messagingSenderId: "960029723357",
+  appId: "1:960029723357:web:99bbc469ffea09115fa934",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
