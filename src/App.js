47fa766f9b8e79e732ace28177eb3bd81dc7b315
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-20 23:24:57 +0900

    user route is been protected

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import DashBoard from "./Pages/DashBoard/DashBoard";
 import MyAppointment from "./Pages/DashBoard/MyAppointment";
 import MyReview from "./Pages/DashBoard/MyReview";
 import Users from "./Pages/DashBoard/Users";
+import RequireAdmin from "./Pages/Login/RequireAdmin";
 
 function App() {
   return (
@@ -39,7 +40,7 @@ function App() {
         >
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
-          <Route path="user" element={<Users></Users>}></Route>
+          <Route path="user" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
         </Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
