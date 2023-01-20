134ecd000f7cf9e8b76130f169e8d43920f694e0
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-21 03:00:53 +0900

    send image to imagebb is complete

--- src/Pages/DashBoard/AddDoctors.js
+++ src/Pages/DashBoard/AddDoctors.js
@@ -1,13 +1,57 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useQueries, useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const AddDoctors = () => {
+    /*
+    3 ways to store images
+    1. third party stroge like imagebb
+    2. your own stronge in your own server
+    3. Datatbase: Mongodb
+
+    for validation a image
+    YUP: to falidate file: search: YUP file validaton for react hook form + limit the size: 
+    */
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
-  const onSubmit = async (data) => {};
+  const { data: services, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/service").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  const imageStorageKey = "cd986efaf6b10a93342b1caa91d1116e";
+
+  const onSubmit = async (data) => {
+    // console.log(data);
+    const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+    .then(res => res.json())
+    .then(result => {
+        if(result) {
+            const img = result.data.url;
+            const doctor = {
+                name: data.name,
+                email: data.email,
+                specialty: data.specialty,
+                img: img
+            }
+            // send to database
+        }
+        console.log(result)
+    })
+  };
   return (
     <div>
       <h2 className="text-3xl">Add a new Doctor</h2>
@@ -71,36 +115,51 @@ const AddDoctors = () => {
         </div>
         <div className="form-control w-full max-w-xs">
           <label className="label">
-            <span className="label-text">Speciality</span>
+            <span className="label-text">Specialty</span>
+          </label>
+          <select
+            {...register("specialty")}
+            className="select w-full max-w-xs"
+          >
+            <option disabled selected>
+              Pick your favorite Simpson
+            </option>
+            {services.map((service) => (
+              <option key={service._id} value={service.name}>
+                {service.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Photo</span>
           </label>
           <input
-            type="text"
-            placeholder="Spaciality"
+            type="file"
+            placeholder="Name"
             className="input input-bordered w-full max-w-xs"
-            {...register("spaciality", {
+            {...register("image", {
               required: {
                 value: true,
-                message: "spacialization is required",
+                message: "Image is required",
               },
             })}
           />
           <label className="label">
-            {errors.password?.type === "required" && (
+            {errors.name?.type === "required" && (
               <span className="label-text-alt text-red-500">
-                {errors.password?.message}
-              </span>
-            )}
-            {errors.password?.type === "minLength" && (
-              <span className="label-text-alt text-red-500">
-                {errors.password?.message}
+                {errors.name?.message}
               </span>
             )}
           </label>
         </div>
+
         <input
           type="submit"
-          value="Add"
-          className="btn uppercase w-full mt-3 font-bold text-white bg-gradient-to-r from-primary to-secondary"
+          value="Add Doctor Information"
+          className="btn uppercase mt-3 font-bold text-white bg-gradient-to-r from-primary to-secondary"
         />
       </form>
     </div>
